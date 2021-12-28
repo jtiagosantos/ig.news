64f6a9c3ba0a76896d70f78464ee82d2ea4ecd3f
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-12-28 00:25:50 -0300

    feat: change SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_API_ID);
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
