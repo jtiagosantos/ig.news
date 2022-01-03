f89b866ad76b601c79b91b7ec5eec4293d743dca
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-02 21:13:11 -0300

    fix: text button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -24,7 +24,7 @@ export function SignInButton() {
       onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
-      Sing in with GitHub
+      Sign in with Github
     </button>
   )
 }
